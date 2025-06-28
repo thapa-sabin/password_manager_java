898c74721ca4e2eb48e159d8532e63621c375fcf
Sabin Thapa <sabin.thapa@bajratechnologies.com>
2025-06-28 22:47:20 +0545

    feat: integrate password generator in password manager app

--- src/com/passwordmanager/app/PasswordManagerApp.java
+++ src/com/passwordmanager/app/PasswordManagerApp.java
@@ -3,10 +3,11 @@
 import java.util.Scanner;
 import com.passwordmanager.core.Vault;
 import com.passwordmanager.models.Credential;
+import com.passwordmanager.utils.PasswordGenerator;
 
  /**
   * This is the main CLI application for the password manager.
-  * Provides a menu option to add, view, delete credentials and (later) to generate passwords as well.
+  * Provides a menu option to add, view, delete credentials and to generate passwords as well.
   */
  public class PasswordManagerApp {
      /**
@@ -23,7 +24,7 @@ import com.passwordmanager.models.Credential;
             System.out.println("1. Add Credential");
             System.out.println("2. View Credential");
             System.out.println("3. Delete Credential");
-            System.out.println("4. Generate Password"); // Yet to implement this
+            System.out.println("4. Generate Password");
             System.out.println("5. List All");
             System.out.println("6. Exit");
             System.out.println("Enter your choice: ");
@@ -36,8 +37,14 @@ import com.passwordmanager.models.Credential;
                         String service = sc.nextLine();
                         System.out.println("Username: ");
                         String username = sc.nextLine();
-                        System.out.println("Password: ");
+                        System.out.println("Password (leave this field blank to generate one): ");
                         String password = sc.nextLine();
+
+                        if(password.isEmpty()){
+                            System.out.println("Generating a new password...");
+                            password = PasswordGenerator.generate(12);
+                            System.out.println("Your password is: " + password);
+                        }
 
                         vault.addCredential(new Credential(service, username, password));
                         break;
@@ -53,6 +60,10 @@ import com.passwordmanager.models.Credential;
                         boolean deleted = vault.deleteByService(del);
                         System.out.println(deleted ? "Deleted" : "Not found.");
                         break;
+                    case "4":
+                        System.out.println("Desired length: ");
+                        int len = Integer.parseInt(sc.nextLine());
+                        System.out.println("Generated Password: " + PasswordGenerator.generate(len));
                     case "5":
                         vault.listAll();
                         break;
